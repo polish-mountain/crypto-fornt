bd6fe304411c76aeba7414ffe553a647e58e930d
alufers <alufers@wp.pl>
2023-10-01 10:26:10 +0200

    Add some styling
    Co-authored-by: Jakub Ostrzołek <kubaostrzolek@gmail.com>

--- src/components/dom/Vulnerabilities.tsx
+++ src/components/dom/Vulnerabilities.tsx
@@ -12,20 +12,21 @@ export default function Vulnerabilities() {
         <motion.div
           initial={{ x: -800 }}
           animate={{ x: 0, transition: { duration: 0.5 } }}
-          className='fixed bg-[#f91114] flex flex-col py-4 px-2 w-[400px] h-[512px] justify-between left-[10vw] gap-8 top-0 bottom-0 my-auto border-black border-2 rounded-xl'>
+          className='fixed bg-[#f9111444] text-gray-300 flex flex-col py-4 px-2 w-[400px] h-[512px] justify-between left-[10vw] gap-8 top-0 bottom-0 my-auto border-black border-2 rounded-xl backdrop-blur'>
           {preview.open_services.map((service, index) => (
             <div key={index} className='flex flex-col gap-4'>
-              <p className='font-bold'>{index + 1}.</p>
+              <h3 className="text-center font-bold text-2xl font-mono text-purple-primary tracking-widest drop-shadow-[0_3.5px_2.5px_rgba(0,0,0,0.8)]">CYBERSECUIRITY THREAT</h3>
+              {/* <p className='font-bold'>{index + 1}.</p> */}
               <h2 className='text- font-bold text-2xl'>
-                Port: <span className='font-normal text-black'>{service.port}</span>
+                Port: <span className='font-normal '>{service.port}</span>
               </h2>
 
               <h2 className='text- font-bold text-2xl'>
-                Service: <span className='font-normal text-black'>{service.proto}</span>
+                Service: <span className='font-normal '>{service.proto}</span>
               </h2>
 
               <h2 className='text- font-bold text-2xl'>
-                Title: <span className='font-normal text-black'>{service.title || '--blank--'}</span>
+                Title: <span className='font-normal'>{service.title || '--blank--'}</span>
               </h2>
             </div>
           ))}
